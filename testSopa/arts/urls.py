b36d78821b2f3d5699fd6061c927e8d223c9059a
tensopa <45923829+tensopa@users.noreply.github.com>
2019-10-14 18:22:37 +0200

    deleting my test files

--- testSopa/arts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import urls
-from . import views
-
-urlpatterns = [
-
-    path('home/', views.arts_list),
-]
--- testSopa/testSopa/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def hello_world(request):
-    return HttpResponse('Hello DropArt users')
