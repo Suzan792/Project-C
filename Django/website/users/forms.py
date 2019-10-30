b56af6c466ee3ae2c9c79ad14fda2c4b9597abff
tensopa <45923829+tensopa@users.noreply.github.com>
2019-10-30 20:59:59 +0100

    user profile information update function created

--- Django/website/users/forms.py
+++ Django/website/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import  UserCreationForm
+from .models import UserProfile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -11,3 +12,17 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields =['first_name','last_name','username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length = 60)
+    last_name = forms.CharField(max_length = 60)
+    email = forms.EmailField(max_length = 60)
+
+    class Meta:
+        model = User
+        fields =['first_name','last_name','username', 'email']
+
+class ProfilePhotoUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['image']
--- Django/website/users/views.py
+++ Django/website/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfilePhotoUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
@@ -18,4 +18,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST,instance=request.user)
+        profile_photo_update_form = ProfilePhotoUpdateForm(request.POST,
+                                                            request.FILES,
+                                                            instance=request.user.userprofile)
+        if user_update_form.is_valid and profile_photo_update_form.is_valid():
+            user_update_form.save()
+            profile_photo_update_form.save()
+            messages.success(request, f'Your profile information is updated')
+            return redirect('profile_page')
+
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_photo_update_form = ProfilePhotoUpdateForm(instance=request.user.userprofile)
+    context ={
+        'user_update_form': user_update_form,
+        'profile_photo_update_form': profile_photo_update_form
+    }
+    return render(request, 'users/profile.html', context)
