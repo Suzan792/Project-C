1022e84aeb9e47679c9811a2396299e00afa4d28
tensopa <45923829+tensopa@users.noreply.github.com>
2019-11-08 23:56:21 +0100

    moved the footer code to base html code so that every page has a footer

--- Django/website/website/views.py
+++ Django/website/website/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from art.models import Artwork
 
 def index(request):
-    Artworks = Artwork.objects.all()[:5]
+    Artwork_list = Artwork.objects.all()
+    paginator = Paginator(Artwork_list, 6)
+    page = request.GET.get('page')
+    Artworks = paginator.get_page(page)
     return render(request,'index.html',{'Artworks':Artworks})
 def contact_page(request):
     return render(request,'contact.html')
