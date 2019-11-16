40648a52ad5a6784aa2172a8ea7b29f32ef612b9
tensopa <45923829+tensopa@users.noreply.github.com>
2019-11-16 14:43:24 +0100

    working like system in progress

--- Django/website/website/settings.py
+++ Django/website/website/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'users',
     'products',
     'crispy_forms',
+    'like_count',
+
 
 ]
 
--- Django/website/website/views.py
+++ Django/website/website/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from art.models import Artwork
+from like_count.models import Like
 
 
 def contact_page(request):
@@ -14,5 +15,31 @@ class ArtListView(ListView):
     paginate_by = 6
     queryset = Artwork.objects.all()
 
-class ArtDetailView(DetailView):
-    model = Artwork
+class ArtDetailView(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user.userprofile
+        object = Artwork.objects.get(pk=self.kwargs.get('pk'))
+        liked = False
+        if Like.objects.filter(user=user).filter(art=object):
+            liked = True
+        context = {
+        'object': object,
+        'liked':liked,
+        }
+        return render(request, 'art/artwork_detail.html', context)
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            art = Artwork.objects.get(pk=self.kwargs.get('pk'))
+            user = request.user.userprofile
+            if Like.objects.filter(user=user).filter(art=art):
+                Like.objects.filter(user=user).filter(art=art).delete()
+                liked = False
+            else:
+                like_instance = Like.objects.create(user=user,art=art)
+                like_instance.save()
+                liked = True
+            context = {
+                'object': art,
+                'liked':liked,
+                }
+            return render(request,'art/artwork_detail.html',context)
