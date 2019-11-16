bee9b567b21517c7aebacbbd1d79a0c7818c0dc6
tensopa <45923829+tensopa@users.noreply.github.com>
2019-11-16 21:21:28 +0100

    improved like button. page doesn't have to be reloaded when liking something

--- Django/website/website/views.py
+++ Django/website/website/views.py
@@ -4,7 +4,8 @@ from django.views.generic import ListView, DetailView, View
 from art.models import Artwork
 from like_count.models import Like
 from django.utils import timezone
-
+import json
+from django.http import JsonResponse
 
 def contact_page(request):
     return render(request,'contact.html')
@@ -28,26 +29,25 @@ class ArtDetailView(View):
         user = request.user.userprofile
         object = Artwork.objects.get(pk=self.kwargs.get('pk'))
         liked = False
+        color = ""
         if Like.objects.filter(user=user).filter(art=object):
             liked = True
+            color = "text-danger"
         context = {
         'object': object,
-        'liked':liked,
+        'color':color,
         }
         return render(request, 'art/artwork_detail.html', context)
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             art = Artwork.objects.get(pk=self.kwargs.get('pk'))
             user = request.user.userprofile
-            if Like.objects.filter(user=user).filter(art=art):
+            if Like.objects.filter(user=user).filter(art=art).exists():
                 Like.objects.filter(user=user).filter(art=art).delete()
                 liked = False
             else:
                 like_instance = Like.objects.create(user=user,art=art)
                 like_instance.save()
                 liked = True
-            context = {
-                'object': art,
-                'liked':liked,
-                }
-            return render(request,'art/artwork_detail.html',context)
+            like_count = Like.objects.filter(art=art).count()
+            return JsonResponse({'liked':liked,'like_count':like_count})
