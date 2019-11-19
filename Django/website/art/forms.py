973b5ad533e6f5b46099a82e9d3d246a3e6b7c91
Farid <faredhattab@gmail.com>
2019-11-19 23:10:53 +0100

    url upload artwork

---
+++ Django/website/art/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+class uploadArt(forms.ModelForm):
+    class Meta:
+        model = models.Artwork
+        fields = ['artwork_name','artwork_description','artwork_photo']
