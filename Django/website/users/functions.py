5bf4458caa4e552acfe82eedc843b0d1962deaa7
Suzan79 <Suzan79@users.noreply.github.com>
2019-11-22 00:34:23 +0100

    change subject of email

--- Django/website/users/functions.py
+++ Django/website/users/functions.py
@@ -13,7 +13,7 @@ from django.core.mail import EmailMessage
 
 def send_confirmation_email(request, form, user, user_email):
     current_site = get_current_site(request)
-    mail_subject = 'Activate your blog account.'
+    mail_subject = 'Activate your account.'
     message = render_to_string('acc_active_email.html', {
         'user': user,
         'domain': current_site.domain,
