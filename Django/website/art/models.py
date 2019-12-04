2a4bc0f7723706016895c0e40fe1f8057005ced3
Farid <faredhattab@gmail.com>
2019-12-04 11:18:54 +0100

    price Misspelled

--- Django/website/art/models.py
+++ Django/website/art/models.py
@@ -13,7 +13,7 @@ class Artwork(models.Model):
     artist = models.ForeignKey(users.UserProfile, on_delete = models.SET('unknown'))
     artwork_description = models.TextField(max_length = 600)
     artwork_likes = models.ManyToManyField(users.UserProfile,related_name="likes",blank=True)
-    artwork_prise = models.DecimalField(max_digits=1000, decimal_places=2,default=24.99)
+    artwork_price = models.DecimalField(max_digits=1000, decimal_places=2,default=24.99)
     artwork_photo = models.ImageField(default='default_art.png',upload_to='art_pics')
     upload_date_time = models.DateField(("Date"), default=datetime.date.today)
 
