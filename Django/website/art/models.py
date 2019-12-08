c9b7289847820b8818a3066cc88c6bc4200b539a
Farid <faredhattab@gmail.com>
2019-12-08 05:46:57 +0100

    comment works on artdetail page + design improved

--- Django/website/art/models.py
+++ Django/website/art/models.py
@@ -32,7 +32,7 @@ class Artwork(models.Model):
 class Comment(models.Model):
     commenter = models.ForeignKey(users.UserProfile, on_delete = models.SET('unknown'))
     artwork = models.ForeignKey(Artwork, on_delete = models.SET('deleted'))
-    comment = models.CharField(max_length = 600)
+    comment = models.TextField(max_length = 500)
     upload_date_time = models.DateField(("Date"), default=datetime.date.today)
 
     def __str__(self):
--- Django/website/art/urls.py
+++ Django/website/art/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import upload_art , artistworkListView, ArtDetailView , deleteArtView , ArtworkUpdateView , CommentDetailView
+from .views import upload_art , artistworkListView, ArtDetailView , deleteArtView , ArtworkUpdateView
 
 urlpatterns = [
 path('art/<int:pk>/', ArtDetailView.as_view(), name='artDetail_page'),
-path('artwork/<int:pk>/', CommentDetailView.as_view(), name='Comment_Detail'),
 path('art/<int:pk>/update/', ArtworkUpdateView.as_view(), name='artUpdate_view'),
 path('art/<int:pk>/delete', deleteArtView.as_view(), name='artDelete_view'),
 path('artistwork/',artistworkListView.as_view(),name = 'artistwork'),
