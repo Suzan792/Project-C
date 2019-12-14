2751f25f36873dfa675db19e5ed0a757b5f02f16
tensopa <45923829+tensopa@users.noreply.github.com>
2019-12-14 21:56:38 +0100

    aws test2

--- Django/website/carts/models.py
+++ Django/website/carts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-import products.models as product
+from products.models import Design
 
 
 class Cart(models.Model):
-    item = models.ManyToManyField(product.Design, null=True, blank=True)
+    item = models.ManyToManyField(Design, null=True, blank=True)
     total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
