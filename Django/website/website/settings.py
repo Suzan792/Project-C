d5fb9784c3115baa0f0dfe5c407746e8745ca20b
tensopa <45923829+tensopa@users.noreply.github.com>
2019-12-16 18:48:50 +0100

    Revert "empty admin static files added"
    This reverts commit cf815e7cdea2c084219b35bd31c5cc9cd117cabf.

--- Django/website/website/settings.py
+++ Django/website/website/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'adminStatic')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -140,8 +139,6 @@ LOGIN_URL= 'login_page'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
