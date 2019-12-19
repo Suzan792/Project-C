6f88e252e39b3b54fe6ccc05f66b73cf4487696a
tensopa <45923829+tensopa@users.noreply.github.com>
2019-12-19 20:57:18 +0100

    more little adjustment

--- Django/website/art/views.py
+++ Django/website/art/views.py
@@ -71,6 +71,7 @@ class ArtDetailView(View):
     def get(self, request, *args, **kwargs):
         art = Artwork.objects.get(pk=self.kwargs.get('pk'))
         designs = Design.objects.filter(art=art, user=None)
+        print(designs.count())
         products = Product.objects.all()
         productWithoutDesign = []
         for product in products:
