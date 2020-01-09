adc4afffa95f59779a448646e999dc1e46caec42
tensopa <45923829+tensopa@users.noreply.github.com>
2020-01-09 18:00:29 +0100

    changed advertisement database model  and advertisement is now clickable

--- Django/website/advertisementSlide/models.py
+++ Django/website/advertisementSlide/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from art.models import Artwork
 
 # Create your models here.
 class AdvertisementSlide(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField()
     image = models.ImageField(upload_to='ads_pics')
+    art = models.ForeignKey( Artwork,on_delete=models.CASCADE,default=None)
