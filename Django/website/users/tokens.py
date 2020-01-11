8add80abc344d90470b1856b35b7b521af163a73
tensopa <45923829+tensopa@users.noreply.github.com>
2020-01-11 15:07:24 +0100

    import six causes server crash. I have commented that line to solve that crash.

--- Django/website/users/tokens.py
+++ Django/website/users/tokens.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six #TODO check if this works
+# from django.utils import six #TODO check if this works
 # import users.django_six as six
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
