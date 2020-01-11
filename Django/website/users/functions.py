6a80593af1d7819fcbd5bd433cc3dd06b8c42a1c
Suzan79 <Suzan79@users.noreply.github.com>
2020-01-11 11:14:20 +0100

    account deactivation

--- Django/website/users/functions.py
+++ Django/website/users/functions.py
@@ -1,13 +1,14 @@
 from django.core.mail import send_mail
-from django.core.exceptions import ValidationError
+from django.contrib import messages
+from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect
 from django.template.loader import render_to_string
-from users.tokens import account_activation_token
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_text
-from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 
+from users.tokens import account_activation_token
 
 def send_confirmation_email(request, form, user, user_email):
     current_site = get_current_site(request)
@@ -25,3 +26,28 @@ def send_confirmation_email(request, form, user, user_email):
         'artdrop.project@gmail.com',
         [user_email],
     )
+
+def deactivate_account(request):
+    '''
+    This function deactivates a logged in user's account and sends an email to the account's owner.
+    '''
+    user = request.user
+    user.is_active = False
+    user.save()
+    
+    user_email = request.user.email
+    current_site = get_current_site(request)
+    mail_subject = 'Your account has been deactivated.'
+    message = 'Your account has been deactivated. If you ever want to reactivate your account, you can send an email to artdrop.project@gmail.com.'
+
+    send_mail(
+        mail_subject,
+        message,
+        'artdrop.project@gmail.com',
+        [user_email],
+    )
+
+    messages.success(request, f'Your account has been deactivated.')
+
+    # logout(request)
+   
