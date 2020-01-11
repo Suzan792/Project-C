cb93cb4e8c1208793fc57f465177a16b5d049fef
Suzan79 <Suzan79@users.noreply.github.com>
2020-01-11 15:47:05 +0100

    Update tokens.py
    used different six

--- Django/website/users/tokens.py
+++ Django/website/users/tokens.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-# from django.utils import six #TODO check if this works
-# import users.django_six as six
+import users.django_six as six
+
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
